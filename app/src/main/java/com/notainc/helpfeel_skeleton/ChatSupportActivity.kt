8b1313aa8fb6fdf1aa7dadffc2171f2435d55b6b
daiiz <da.iz.mg@gmail.com>
2019-04-15 22:19:58 +0900

    ChatSupportActivity

---
+++ app/src/main/java/com/notainc/helpfeel_skeleton/ChatSupportActivity.kt
@@ -0,0 +1,11 @@
+package com.notainc.helpfeel_skeleton
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ChatSupportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
